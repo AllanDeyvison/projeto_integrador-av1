a836b68c223be1bca86a64f48db35b1ab3fd6d50
AllanDeyvison <allan.13dias@gmail.com>
2024-06-24 23:04:22 -0300

    first commit

---
+++ js/mapa.js
@@ -0,0 +1,11 @@
+
+let mapa = document.querySelector("#mapa")
+
+function mapaZoom(){
+    mapa.style.width = "1000px"
+    mapa.style.height = "900px"
+}
+function mapaNormal(){
+    mapa.style.width = "400px"
+    mapa.style.height = "250px"
+}
